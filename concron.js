24417f541c124e30a497192918780f2ce8eda13d
procencephalon <fredrsat@gmail.com>
2016-10-14 16:11:55 +0200

    added support for game id caching and news filters
    Added support for caching game ids (for use for those with privat
    accounts etc)
    Added better error handling
    Added support for different filter types for the news feed
    Improved cron runner of console

---
+++ concron.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+
+'use strict';
+
+/*
+ *
+ * Console runner for use with naitive cron
+ *
+ */
+
+const feedmaker = require('./feedmaker.js');
+const program = require('commander');
+
+program
+  .version('0.0.1')
+  .arguments('<userid> <filePathFeed>')
+  .usage('<userid> <filepath> [options]')
+  .option('-s, --save [filepath]', 'Saves game id\'s to file')
+  .option('-l, --load [filepath]', 'Loades id\'s from file and use it as source')
+  .option('-f, --filter [filter]', 'Defines filter to use on Steam news')
+  .action((userid, filepath, options) => {
+
+    //Create feed
+    feedmaker(userid, filepath, {
+      save: program.save,
+      load: program.load,
+      filter:program.filter
+    }); 
+  })
+  .parse(process.argv);
+
+
+
+
--- feedmaker.js
+++ feedmaker.js
@@ -2,7 +2,7 @@
  *
  * Steam RSS maker by Procencephalon 
  * 
- */
+ */ 
 
 'use strict';
 
@@ -10,6 +10,10 @@ const builder = require('xmlbuilder'),
       request = require('request'),
       cheerio = require('cheerio'),
       fs = require('fs');
+
+/*
+ * Helper function to sum up all matches of a regex
+ */
 
 let getMatches = (string, regex, index) => {
   index || (index = 1); 
@@ -24,73 +28,133 @@ let getMatches = (string, regex, index) => {
 /*
  *
  * Creates a RRS feed from all the lates updates from all the games in a user's public Steam profile    
- * @param{String} userid A user's id from steamcommunity.com
+ * @param{String} userid   A user's id from steamcommunity.com
+ * @param{String} filepath Path where to save the *.rss file
+ * @param{Object} options:
+ *                options.save   If and where to save game ids from profile
+ *                options.load   If and where from to load cached game ids 
+ *                options.filter Filter to use for news feed. 
  *
  */
 
-module.exports = (userid, filepath) => { 
-  let list = [], promises = [], games = [];
+module.exports = (userid, filepath, options) => { 
+  let list = [], promises = [], games = [],
+  filter = options.filter ? '&feed=' + options.filter : '';
 
   //Find all games on the users public profile
   (new Promise ((resolve, reject) => {
-    request('https://steamcommunity.com/profiles/' + userid + '/games/?tab=all', (error, response, body) => {
-      if (error && response.statusCode !== 200) return reject(response.statusCode);
 
-      let $ = cheerio.load(body);
-      games = getMatches($('script').text(), /\"appid\":([0-9]*)/g);
-
-      resolve();
-    });  
-  })).then(done => {
-
-    //Iterate through all games and collect required information for RSS items
-    games.forEach(appid => {
-      promises.push(
-        new Promise((resolve, reject) => { 
-          request('http://store.steampowered.com/news/?appids=' + appid + '&feed=steam_updates', (error, response, body) => {
-            if (error && response.statusCode !== 200) reject(response.statusCode);
-
-            let $ = cheerio.load(body);
-            $('#mainBlock').find('div .newsPostBlock').each( (index, element) => {
-              let url = $(element).find('.headline a');
-              
-              list.push({"item":{
-                title: $(url).text(), 
-                description: $(element).find('.body').text().trim(),
-                link: $(url).attr('href'),
-                pubDate:new Date($(element).find('div .date').text()).toUTCString()
-              }}); 
-            });
-            resolve(appid);
-          });
-        })
-      );
-    });
-
-    //Create a RSS file of all updates
-    Promise.all(promises).then(
-      values => { 
-        let feed = builder.create('rss')
-            .att('version', '2.0')
-            .ele('channel')
-              .ele('title', 'Game updates for user ' + userid).up()
-              .ele('description', 'Feed that consolidates all games updates for a given user').up()
-              .ele('link', 'http://127.0.0.1').up() 
-              .ele('lastBuildDate', new Date().toUTCString()).up() 
-              .ele(list)
-            .end({ pretty: true});
-
-        fs.writeFile(filepath, feed, function(err) {
-          if(err) {
-            return console.log('Error ' + err);
-          }
-          console.log("RSS sucessfully created");
+    //Load from cashed game id's file if option for load is set
+    if(options.load){
+      fs.readFile(options.load, 'utf8', (err, data) => {
+        if(err){
+          reject('Error loading game ids ' + err) 
+          return;
+        };
+        
+        data.split(',').forEach(id => {
+          games.push(id);
         });
-      }, 
-      reason => {
-        console.log('Error ' + reason);
-      }
-    );
-  });
+
+        resolve();
+      });
+    }else{
+
+      //Load from steam profile
+      request('https://steamcommunity.com/profiles/' + userid + '/games/?tab=all', (error, response, body) => {
+        if (error && (response && response.statusCode !== 200)){
+          reject(response.statusCode);
+          return;
+        }
+        
+        let $ = cheerio.load(body);
+        games = getMatches($('script').text(), /\"appid\":([0-9]*)/g);
+
+        resolve();
+      });  
+    }
+  })).then(
+    done => {
+
+      //Iterate through all games and collect required information for RSS items
+      games.forEach(appid => {
+        promises.push(
+          new Promise((resolve, reject) => { 
+            request('http://store.steampowered.com/news/?appids=' + appid + filter, (error, response, body) => {
+              if (error && (response && response.statusCode !== 200)){
+                reject(response.statusCode);
+                return;
+              } 
+
+              let $ = cheerio.load(body);
+              $('#mainBlock').find('div .newsPostBlock').each( (index, element) => {
+                let url = $(element).find('.headline a');
+                
+                list.push({"item":{
+                  title: $(url).text(), 
+                  description: $(element).find('.body').text().trim(),
+                  link: $(url).attr('href'),
+                  //FIX:pubDate:new Date($(element).find('div .date').text()).toUTCString()
+                }}); 
+              });
+              resolve(appid);
+            });
+          })
+        );
+      });
+
+      //Create a RSS file of all updates
+      Promise.all(promises).then(
+        values => { 
+          if(filepath && list.length > 0){
+            let feed, now = new Date();
+
+            try{
+              feed = 
+              builder.create('rss')
+                .att('version', '2.0')
+                .ele('channel')
+                  .ele('title', 'Game updates for user ' + userid).up()
+                  .ele('description', 'Feed that consolidates all games news/updates for a given user').up()
+                  .ele('link', 'http://127.0.0.1').up() 
+                  .ele('lastBuildDate', new Date(now.getTime() + now.getTimezoneOffset()).toString()).up() //Fri, 14 Oct 2016 13:41:22 GMT
+                  .ele(list)
+                .end({ pretty: true});
+
+              //Save RSS feed to file          
+              fs.writeFile(filepath, feed, err => {
+                if(err)throw err;
+    
+                console.log('RSS feed sucessfully created with filter: ' + (options.filter ? options.filter : 'none') );
+              });
+            }catch(err){
+                console.log('Error ' + err);  
+                return;
+            }
+          }else{
+            console.log('Warning: RSS filepath not given or no results found');  
+          }
+
+          //Save game id's to file if option for save is set
+          if(options.save){
+              fs.writeFile(options.save, games, err => {
+              if(err) {
+                console.log('Error ' + err);
+                return;
+              }
+
+              console.log('Game id\'s saved sucessfully');
+            });
+          }
+        }, 
+        reason => {
+          console.log('Error ' + reason);
+        }
+      );
+    },
+    reason => {
+      console.log('Error ' + reason);  
+    }
+  );
 }
 
